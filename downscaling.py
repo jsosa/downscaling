0c5e4e8d0d38389daabf07b1f147602e17aa2e64
jsosa <j.sosa@bristol.ac.uk>
2018-11-20 15:41:36 +0100

    Default options added

--- downscaling.py
+++ downscaling.py
@@ -54,4 +54,4 @@ wsl_lresf = './test/wsl_lres.elev'
 dem_hresf = './test/dem.tif'
 nodata    = -9999 # NODATA value in both data sets
 
-main(dem_hresf,wsl_lresf,wd_hresf,nodata)
+main(dem_hresf=dem_hresf,wsl_lresf=wsl_lresf,wd_hresf=wd_hresf,nodata=nodata)

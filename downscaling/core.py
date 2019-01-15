26c19ac7fd664dcf5f68bb886d2abd9d6cf27454
jsosa <j.sosa@bristol.ac.uk>
2019-01-15 14:18:13 +0100

    xarray version

--- downscaling/core.py
+++ downscaling/core.py
@@ -5,13 +5,12 @@
 # mail: sosa.jeison@gmail.com / j.sosa@bristol.ac.uk
 
 import os
-import sys
-import shutil
+import rasterio as rio
+import xarray as xr
 import numpy as np
-import gdalutils as gu
 from subprocess import call
 
-def downscaling(dem_hresf,msk_hresf,wsl_lresf,wd_lresf,thr_val,thr_dpt,thr_decay,nodata,wd_hresf):
+def downscaling(dem_hresf,msk_hresf,wsl_lresf,wd_lresf,thr_val,thr_dpt,thr_decay,wd_hresf,gtiff=True):
 
     """
     Downscale flood maps from coarse to a high resolution. Water
@@ -27,49 +26,35 @@ def downscaling(dem_hresf,msk_hresf,wsl_lresf,wd_lresf,thr_val,thr_dpt,thr_decay
     """
 
     # Reading coarse resolution water surface elevation
-    wsl_lres = gu.get_data(wsl_lresf)
-    
+    wsl_lres = xr.open_rasterio(wsl_lresf).sel(band=1)
+
     # Reading coarse resolution water depth
-    wd_lres = gu.get_data(wd_lresf)
+    wd_lres = xr.open_rasterio(wd_lresf).sel(band=1)
 
     # Reading geo information for both
-    geo_hres = gu.get_geo(dem_hresf)
-    geo_lres = gu.get_geo(wsl_lresf)
+    dem_hres = xr.open_rasterio(dem_hresf).sel(band=1)
 
     # Selecting water depth > thr_dpt
-    iy,ix = np.where(wd_lres>thr_dpt)
-    x_lres = geo_lres[8][ix]
-    y_lres = geo_lres[9][iy]
+    wsl_lres_msk = wsl_lres.where(wd_lres>thr_dpt) #thr_dpt
+    wsl_lres_msk = wsl_lres_msk.to_series().to_frame().dropna().reset_index(level=['x','y'])
+    wsl_lres_msk['xmin'] = wsl_lres_msk['x'] - thr_val
+    wsl_lres_msk['ymin'] = wsl_lres_msk['y'] - thr_val
+    wsl_lres_msk['xmax'] = wsl_lres_msk['x'] + thr_val
+    wsl_lres_msk['ymax'] = wsl_lres_msk['y'] + thr_val
 
     # Create a empty array, the output array
-    wd_hres = np.zeros((len(geo_hres[9]),len(geo_hres[8])))
+    wd_hres = xr.full_like(dem_hres,fill_value=0)
+    x_hres = wd_hres.x
+    y_hres = wd_hres.y
 
-    # Iterate over every coarse water surface elevation pixel
-    for i in range(len(x_lres)):
+    for xmin,ymin,xmax,ymax,wsl in zip(wsl_lres_msk['xmin'],wsl_lres_msk['ymin'],
+                                       wsl_lres_msk['xmax'],wsl_lres_msk['ymax'],
+                                       wsl_lres_msk[0]):
 
-        # Searching window over the pixel
-        xmin = x_lres[i] - thr_val
-        ymin = y_lres[i] - thr_val
-        xmax = x_lres[i] + thr_val
-        ymax = y_lres[i] + thr_val
-
-        # Clipping the high resolution DEM for previous window
-        dat_dem, geo_dem = gu.clip_raster(dem_hresf, xmin, ymin, xmax, ymax)
-
-        # Getting indexes from high resolution DEM
-        yind0,yind1,xind0,xind1 = get_index_geo(geo_dem[8],geo_dem[9],geo_hres[8],geo_hres[9])
-        
-        # Reading 'from previous iteration' high resolution water depth values
-        win = wd_hres[yind0:yind1,xind0:xind1]
-
-        # Getting water surface elevation value
-        # Substracting water surface eleavation to values in DEM window
-        # Only positive values are valid cells
-        wd = wsl_lres[iy[i],ix[i]]-dat_dem
-        wd[wd<0]=0        
-
-        # Average previous values
-        wd_hres[yind0:yind1,xind0:xind1] = (win+wd)/2 # np.mean([win,wd],axis=0)        
+        wd = wsl-dem_hres.sel(x=slice(xmin,xmax),y=slice(ymax,ymin))
+        wd = wd.where(wd>0,0)
+        win = wd_hres.sel(x=slice(xmin,xmax),y=slice(ymax,ymin))
+        wd_hres.loc[dict(x=x_hres[(x_hres>xmin)&(x_hres<xmax)], y=y_hres[(y_hres>ymin)&(y_hres<ymax)])] = ((wd+win)/2).values
 
     # Apply a exonential decay over the inundated area
     # Create a temp folder
@@ -81,26 +66,28 @@ def downscaling(dem_hresf,msk_hresf,wsl_lresf,wd_lresf,thr_val,thr_dpt,thr_decay
 
     # Calc proximity around `msk_hresf`
     call(['gdal_proximity.py','-distunits','GEO',
-                              '-co','COMPRESS=LZW',
-                              '-nodata','0',
-                              msk_hresf,
-                              outfolder+'buffer_dist.tif'])
+                            '-co','COMPRESS=LZW',
+                            '-nodata','0',
+                            msk_hresf,
+                            outfolder+'buffer_dist.tif'])
 
     # Multiply by decay function
-    mask = gu.get_data(outfolder+'buffer_dist.tif')
-    wd_hres = wd_hres * (1-mask)**thr_decay
+    mask = xr.open_rasterio(outfolder+'buffer_dist.tif').sel(band=1)
+    wd_hres[:,:] = wd_hres[:,:] * ((1-mask)**thr_decay).data
 
     # Write final high resolution water depth map
-    gu.write_raster(wd_hres,wd_hresf,geo_hres,'Float64',0)
+    xr.Dataset({'wd':wd_hres}).to_netcdf(wd_hresf,encoding={'wd':{'zlib':True}})
 
-def get_index_geo(x1,y1,x2,y2):
-    
-    xmin  = x1.min()
-    ymin  = y1.min()
-    xmax  = x1.max()
-    ymax  = y1.max()
-    xind0 = abs(x2-xmin).argmin()
-    xind1 = abs(x2-xmax).argmin()+1  # inclusive slicing
-    yind0 = abs(y2-ymax).argmin()
-    yind1 = abs(y2-ymin).argmin()+1  # inclusive slicing
-    return yind0,yind1,xind0,xind1
+    # Output GTIFF file
+    if gtiff:
+        name = os.path.dirname(wd_hresf) + '/' + os.path.basename(wd_hresf).split('.')[0] + '.tif'
+        with rio.open(name, 'w',
+                driver ='GTiff',
+                height = len(wd_hres.y),
+                width = len(wd_hres.x),
+                count=1,
+                dtype=str(wd_hres.dtype),
+                crs=wd_hres.crs,
+                compress='lzw',
+                transform=wd_hres.transform[0:6]) as dst:
+            dst.write(wd_hres.data,1)
